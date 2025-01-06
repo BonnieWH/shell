447277ea1ffc59487e9766128f464574577404c6
bonniewh <bonniewh@proton.me>
2025-01-05 18:22:59 -0800

    Trying this out to try to get some auto grading-- I don't think it's quite right yet

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -30,7 +30,7 @@ mv ./rawdata ./data/raw
 ls ./data/raw
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
-mkdir ./data/processed
+mkdir ./data processed 
 mkdir ./data/processed server_logs user_logs event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
@@ -41,9 +41,12 @@ cp ./data/raw/*user*.log ./data/processed/user_logs
 cp ./data/raw/*event*.log ./data/processed/event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
+rm ./data/raw/*ipadddr*
+rm ./data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-
+touch ./data/inventory.txt
+ls -r ./data/processed > ./data/inventory.txt
 
 
 ###########################################
